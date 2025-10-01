e9b64001eb23b07c8cf2ebd682da55a53ff880ad
David Rios <david.rios.valencia.al@iespoligonosur.org>
2025-10-01 17:56:21 +0200

    TrabajoTerminado

---
+++ Animales/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Animal
+from .models import Protectora
+from .models import Colaborador
+
+admin.site.register(Animal)
+admin.site.register(Protectora)
+admin.site.register(Colaborador)
