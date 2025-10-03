d1120d235bc2c07744a25e39149b8a4c197fe04a
David <david.rios.valencia.al@iespoligonosur.org>
2025-10-03 11:47:45 +0200

    RamaArreglada

--- Animales/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from.import views
-
-urlpatterns = [
-        path('',views.index,name='index.html'),
-        path('animal',views.animal_list, name='Animal_list'),
-        path('protectora',views.protectora_list, name='Protectora_list'),
-        path('colaborador',views.colaborador_list, name='Colaborador_list'),
-]       
